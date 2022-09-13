5e79c0cc960fea567d963f466333ef8bb53924bc
Carrol2709 <111042693+Carrol2709@users.noreply.github.com>
2022-09-13 13:40:17 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,22 @@
 #include "holberton.h"
 
 /**
- 
- * main - Entry point
- 
+ * main - print Holberton
  *
- 
  * Return: Always 0 (Success)
- 
- *
- 
  */
 
 int main(void)
-
 {
-
- char *sh = "Holberton";
-
-   while (*sh)
-
-   {
-
- _putchar(*sh);
-
- sh++;
-    }
-
-   _putchar('\n');
-
-   return (0);         
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
